8b42bc5da53401e504d4c585897a47a3628ae4dc
mariarthamanuaba <mariarthamanuaba@gmail.com>
2026-05-31 01:07:10 +0800

    Update bot_test.py

--- bot_test.py
+++ bot_test.py
@@ -4,6 +4,13 @@ import os
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
+print("TOKEN:", TOKEN)
+print("CHAT_ID:", CHAT_ID)
+
+if not TOKEN or not CHAT_ID:
+    print("ENV ERROR: TOKEN atau CHAT_ID kosong")
+    exit()
+
 message = "Bot XAU Signal Aktif 🚀"
 
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
@@ -13,6 +20,6 @@ payload = {
     "text": message
 }
 
-requests.post(url, data=payload)
+response = requests.post(url, data=payload)
 
-print("Message sent")
+print("Response:", response.text)
